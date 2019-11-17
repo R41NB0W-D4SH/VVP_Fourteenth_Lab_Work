bc8abe78ea981f84733ee74eb075eec355b747bf
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-17 15:32:31 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int n, i, j;
+	cout << "Введите размерность массива: ";
+	cin >> n;
+
+	int *arr = new int[n];
+
+	for (i = 0; i < n; i++)
+	{
+		cout << "Введите элемент массива: ";
+		cin >> arr[i];
+	}
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j < n; j++)
+		{ 
+			if (arr[i] == arr[j])
+				break;
+		}
+		if (arr[i] = arr[j])
+		{
+			break;
+		}
+	}
+
+	cout << "Одинаковые элементы массива: " << i << ' ' << j;
+
+	system("pause");
+	return 0;
+}
