d26ba9c8f0d8f815ca7866066e5a5f036aa36a90
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-17 15:16:51 +0300

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int i, n, min;
+	cout << "Введите размерность массива: ";
+	cin >> n;
+
+	int *arr = new int[n];
+
+	for (i = 0; i < n; i++)
+	{
+		cout << "Введите элемент массива: ";
+		cin >> arr[i];
+	}
+
+	min = arr[0];
+
+	for (i = 2; i < n; i = i + 2)
+	{
+		min = arr[i] < min ? arr[i] : min;
+	}
+
+	cout << "Минимальный элемент массива: " << min;
+
+	system("pause");
+	return 0;
+}
