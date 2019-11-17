19777bf95645f0c2b25dbc6209d9fcaf510f10cb
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-11-17 15:09:15 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int i, n, d;
+	cout << "Введите размерность массива: ";
+	cin >> n;
+
+	int *arr = new int[n];
+
+	for (i = 0; i < n; i++) 
+	{
+		cout << "Введите элемент массива: ";
+		cin >> arr[i];
+	}
+
+	d = arr[1] - arr[0];
+
+	for (i = 2; i < n; i++) 
+	{
+		if (arr[i] - arr[i - 1] != d) 
+		{
+			d = 0;
+			break;
+		}
+	}
+
+	cout << "Разность арифметической прогрессии: " << d;
+
+	system("pause");
+	return 0;
+}
